3faf7bfc6bdd135c97bd0b90fbf10d0c864d1c49
Berat <bharurluoglu@hotmail.com>
2023-01-28 20:35:03 +0300

    Add Product Seeder, Factory, Migration

---
+++ source/database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::truncate();
+        Product::factory()->count(1000)->create();
+    }
+}
